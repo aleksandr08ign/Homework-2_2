ac45be7da038fa8b037616d65a2f7be25d277500
Александр <ignatenko.ap@yandex.ru>
2023-08-06 20:16:53 +0300

    hw2_2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задача 1.");
-        int a = 1_200_000_000;
-        byte b = 120;
-        short c = 32_000;
-        long d = 9_120_999_999_999;
-        System.out.println("Значение переменной 'a' с типом int равно" + a);
-        System.out.println("Значение переменной 'b' с типом byte равно" + a);
-        System.out.println("Значение переменной 'c' с типом short равно" + a);
-        System.out.println("Значение переменной 'd' с типом long равно" + a);
-    }
+        System.out.println("Домашнее задание 2.2");
+            }
 }
